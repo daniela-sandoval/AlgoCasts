b1b51af4dbcf7d834a57145037e197cae5ef87fb
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-11-03 22:40:08 -0500

    deleted the length check

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,10 +8,23 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+// Broken down steps for solution
+// 1. Establish counters
+// 2. Loop through first array to make the first counter
+//   a. Instead of an IF statement block, use shorthand/ternary
+//   b. Consider where you want to convert to lowercase and test for ALPHABETICAL values
+// 3. Repeat the same step for the second array and second counter
+// 4. Create final loop that will check to see if letters from the FIRST input are in the second based on TWO conditions
+//   a. Whether or not the key/letter exists in the second string
+//   b. Making sure the frequency of that letter (once it does exist) is the same in both strings
+// 5. If the all letters pass the test, return true that it is a valid anagram.
+// FUTURE WORRIES: Making this dry by having to lowercase twice
+
+
 function anagrams(stringA, stringB) {
-  if(stringA.length !== stringB.length) {
-    return false
-  }
+  // if(stringA.length !== stringB.length) {
+  //   return false
+  // }
   let frequencyCounter1 = {}
   let frequencyCounter2 = {}
   for(let letter of stringA) {
@@ -79,16 +92,6 @@ function anagrams(stringA, stringB) {
   //   return true
   // }
 
-// Broken down steps for solution
-// 1. Establish counters
-// 2. Loop through first array to make the first counter
-//   a. Instead of an IF statement block, use shorthand/ternary
-//   b. Consider where you want to convert to lowercase and test for ALPHABETICAL values
-// 3. Repeat the same step for the second array and second counter
-// 4. Create final loop that will check to see if letters from the FIRST input are in the second based on TWO conditions
-//   a. Whether or not the key/letter exists in the second string
-//   b. Making sure the frequency of that letter (once it does exist) is the same in both strings
-// 5. If the all letters pass the test, return true that it is a valid anagram.
 
 
 module.exports = anagrams;
