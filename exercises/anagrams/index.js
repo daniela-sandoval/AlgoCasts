0c1e2ed80ed932f3699f5cd80c4861d3298be4ab
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-11-03 22:21:15 -0500

    rough solution for anagra

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -10,7 +10,44 @@
 
 // Using frequency counter method
 function anagrams(stringA, stringB) {
-  
+  if(stringA.length !== stringB.length) {
+    return false
+  }
+  // establish counters
+  let frequencyCounter1 = {}
+  let frequencyCounter2 = {}
+  // loop through to make counter one
+  for(let letter of stringA) {
+    // instead of an if statement block...
+    if(/[a-z]/.test(letter)) {
+      let char = letter.toLowerCase()
+      frequencyCounter1[char] = (frequencyCounter1[char] || 0) + 1
+    }
+  }
+  // loop through to make counter two
+  for(let letter of stringB) {
+    // instead of an if statement block...
+    if(/[a-z]/.test(letter)) {
+      let char = letter.toLowerCase()
+      frequencyCounter2[char] = (frequencyCounter2[char] || 0) + 1
+    }
+  }
+  console.log(frequencyCounter1)
+  console.log(frequencyCounter2)
+  // check to see if all letters from stringA counter were used in stringB counter
+  for(let key of Object.keys(frequencyCounter1)) {
+    // 1. compare the keys between counters
+    if(!frequencyCounter2[key]) {
+      return false
+    }
+    // 2. compare the value of the keys (frequency)
+    if (frequencyCounter1[key] !== frequencyCounter2[key]) {
+      return false
+    }
+
+  }
+  // Return true if it was found as a key in our counters AND it had the same frequency
+  return true
 }
 
 module.exports = anagrams;
