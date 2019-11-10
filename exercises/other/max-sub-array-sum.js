6eb5708d55066a991eded38c497654d2826b54ac
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-11-10 15:27:00 -0500

    added o(n) solution

--- exercises/other/max-sub-array-sum.js
+++ exercises/other/max-sub-array-sum.js
@@ -1,4 +1,45 @@
 // Write a function called maxSubarraySum which accepts an array of integers and a number called n. The function should calculate the max. sum of n consecutive elements in the array.
+// maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2) -> 10
+
+// Colt's Solution: O(n)
+function maxSubarraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  // Edge case
+  if (arr.length < num) return null;
+  // First window
+  for(let i = 0; i < num; i ++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum
+  // Loop that will shift the window
+  for(let i = num; i < arr.length; i++) {
+    // takes out the first element arr[i - num] in the window and it adds the new element arr[i]
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+
+// Naive Solution: O(N^2)
+function maxSubarraySum(arr, num) {
+  if (num > arr.length) {
+    return null
+  }
+  let max = -Infinity;
+  for (let i = 0; i < arr.length - num + 1; i ++) {
+    temp = 0;
+    for (let j = 0; j < num; j++) {
+      temp += arr[i + j];
+    }
+    if (temp > max) {
+      max = temp;
+    }
+  }
+  return max
+}
+
+// My Attempts...
 function maxSubarraySum(arr, num) {
   let max = []
   let left = 0
@@ -12,6 +53,7 @@ function maxSubarraySum(arr, num) {
   }
   return max[max.length -1]
 }
+
 function maxSubarraySum(arr, num) {
   let max = []
   let left = 0
